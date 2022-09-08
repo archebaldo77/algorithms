24186c12b88df44f6a437263a5dcce39f2266d35
archebaldo <zakharov90@gmail.com>
2022-09-08 22:27:01 +0300

    Add algorithm - Bubble sort

---
+++ 05-bubble-sort.js
@@ -0,0 +1,15 @@
+// Сложность O(n*n);
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j + 1] < arr[j]) {
+        const tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+
+  return arr;
+};
