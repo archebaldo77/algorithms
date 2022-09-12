565af782ed2e431aa803aaa854e1b9b568e41f2b
archebaldo <zakharov90@gmail.com>
2022-09-12 22:14:26 +0300

    Add algorithm - Quick sort

---
+++ 06-quick-sort.js
@@ -0,0 +1,26 @@
+// Сложность O(log(n) * n);
+
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivotIndex = Math.floor(arr.length / 2);
+  let pivot = arr[pivotIndex];
+  let less = [];
+  let greater = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === pivotIndex) {
+      continue;
+    }
+
+    if (arr[i] < pivot) {
+      less.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(less), pivot, ...quickSort(greater)];
+};
